2faf3a234a92e77cd72c20dd06906b0078ffdb48
Natalie Malmgren <nmalmgren@sandiego.edu>
2021-04-19 16:58:24 -0700

    added name confirmation and updating balance

--- __init__.py
+++ __init__.py
@@ -9,21 +9,37 @@ class CheckBalance(MycroftSkill):
 
     @intent_file_handler('balance.check.intent')
     def handle_balance_check(self, message):
-        
-        n = int(self.get_response('What is your Customer I.D?'))
-
         database1 = "cubic.sql"
 
         # create a database connection
         conn = sqlite3.connect(database1)
         
         cur = conn.cursor()
+        
+        answer = "no"
+        while (answer == "no"):
+            n = int(self.get_response('What is your Customer I.D?'))
 
+            cur.execute("SELECT * FROM Customer WHERE CustomerID = ?", (n,))
+            cust = cur.fetchone()
 
-        cur.execute("SELECT * FROM Customer WHERE CustomerID = ?", (n,))
-        cust = cur.fetchone()
+            answer = self.ask_yesno('You are requesting the account balance for {}. Is this you? (yes/no)'.format(cust[2]))
+    
+        
 
         self.speak('Your balance is ${}.'.format(cust[3]))
+        answer = self.ask_yesno('Would you like to add money to your account? (yes/no)')
+
+        if( answer == "yes" )
+            balance = float(self.get_response("How much money would you like to add?"))
+            balance += cust[3]
+            cur.execute("UPDATE Customer SET Balance = ? WHERE CustomerID = ?", (balance, n,))
+        else
+            self.speak("Have a great day!")
+
+
+
+
 
         #self.speak_dialog('balance.check')
         conn.close()
